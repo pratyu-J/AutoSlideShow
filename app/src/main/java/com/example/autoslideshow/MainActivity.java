ec1c88c7cc956da6535b5ee5d29446ac51af9989
pratyu-j <pratyush021j@gmail.com>
2020-04-27 05:31:42 +0530

    fix - orientation problem

--- app/src/main/java/com/example/autoslideshow/MainActivity.java
+++ app/src/main/java/com/example/autoslideshow/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.autoslideshow;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+//import android.widget.LinearLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
@@ -30,6 +37,7 @@ import com.smarteist.autoimageslider.SliderView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,23 +47,12 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
     int count = 0;
 
 
-/*        sliderView = findViewById(R.id.imageSlider);
-
-        adapter = new SliderAdapter(this);
-        sliderView.setSliderAdapter(adapter);
-
-        sliderView.setIndicatorAnimation(IndicatorAnimations.THIN_WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderView.setSliderTransformAnimation(SliderAnimations.POPTRANSFORMATION);
-        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
-        sliderView.setIndicatorSelectedColor(Color.WHITE);
-        sliderView.setIndicatorUnselectedColor(Color.GRAY);
-        sliderView.setScrollTimeInSec(5);
-        sliderView.setAutoCycle(true);*/
-        //sliderView.startAutoCycle();
 
         SliderLayout sliderLayout;
         HashMap<String, Uri> Hash_file_maps = new HashMap<>();
+        ArrayList<Uri> imgurl = new ArrayList<>();
         Button toGallery;
+        CardView card;
 
         @Override
         protected void onCreate (Bundle savedInstanceState){
@@ -65,8 +62,14 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
             setContentView(R.layout.activity_main);
 
             //Hash_file_maps = new HashMap<String, Bitmap>();
+            //card = findViewById(R.id.card);
 
             sliderLayout = (SliderLayout) findViewById(R.id.slider);
+           /* SliderLayout sliderLayout = new SliderLayout(this);
+            LinearLayout.LayoutParams orientationparam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 550);
+            orientationparam.setMargins(5,5,5,5);
+            SliderLayout.setLayoutParams(orientationparam);
+            card.addView(SliderLayout);*/
 
             sliderLayout.setPresetTransformer(SliderLayout.Transformer.Accordion);
             sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
@@ -114,7 +117,12 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
             sliderLayout.setDuration(3000);
             sliderLayout.addOnPageChangeListener(this);*/
         }
-            @Override
+
+
+
+
+
+    @Override
         protected void onStop() {
 
             sliderLayout.stopAutoCycle();
@@ -166,16 +174,13 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
                 Log.d("mainActivity", "we r cliping data");
                 for (int i = 0; i < clipData.getItemCount(); i++) {
                     Log.d("mainActivity2", "inside for");
-                    Uri imgurl = clipData.getItemAt(i).getUri();
-                    try {
-                        InputStream is = getContentResolver().openInputStream(imgurl);
-                        Bitmap bitmap = BitmapFactory.decodeStream(is);
-                        bm.add(bitmap);
+                    //Uri imgurl = clipData.getItemAt(i).getUri();
+                    imgurl.add(clipData.getItemAt(i).getUri());
+                        //InputStream is = getContentResolver().openInputStream(imgurl);
+                        //Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        //bm.add(bitmap);
                         Hash_file_maps.put(String.valueOf(count++), clipData.getItemAt(i).getUri());
 
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
                 }
                 for (String name : Hash_file_maps.keySet()) {
 
@@ -195,18 +200,22 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
                 sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
                 sliderLayout.setCustomAnimation(new DescriptionAnimation());
                 sliderLayout.startAutoCycle();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    sliderLayout.setLayoutMode(ViewGroup.LAYOUT_MODE_CLIP_BOUNDS);
+                }
                 sliderLayout.setDuration(5000);
                 sliderLayout.addOnPageChangeListener(this);
 
             }
             else {
-                Uri imageUrl  = data.getData();
+                //Uri imageUrl  = data.getData();
+                imgurl.add(data.getData());
 
-                try {
-                    InputStream is = getContentResolver().openInputStream(imageUrl);
-                    Bitmap bitmap = BitmapFactory.decodeStream(is);
-                    bm.add(bitmap);
-                    Hash_file_maps.put(String.valueOf(count++), clipData.getItemAt(0).getUri());
+                //try {
+                    //InputStream is = getContentResolver().openInputStream(imageUrl);
+                    //Bitmap bitmap = BitmapFactory.decodeStream(is);
+                   // bm.add(bitmap);
+                    Hash_file_maps.put(String.valueOf(count++), imgurl.get(0));
 
                     for (String name : Hash_file_maps.keySet()) {
 
@@ -228,9 +237,9 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
                     sliderLayout.setDuration(3000);
                     sliderLayout.addOnPageChangeListener(this);
 
-                } catch (FileNotFoundException e) {
+                /*} catch (FileNotFoundException e) {
                     e.printStackTrace();
-                    }
+                    }*/
                 }
 
 
@@ -244,6 +253,29 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
                                 adapter.renewItems(sliderItemList);*/
                             }
                         }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+           // SliderLayout sliderLayout = new SliderLayout(this);
+            LinearLayout.LayoutParams orientationparam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            orientationparam.setMargins(5,5,5,5);
+            sliderLayout.setLayoutParams(orientationparam);
+           // card.addView(sliderLayout);
+
+
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+          //  SliderLayout sliderLayout = new SliderLayout(this);
+            LinearLayout.LayoutParams orientationparam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 1500);
+            orientationparam.setMargins(0,0,0,0);
+            sliderLayout.setLayoutParams(orientationparam);
+           // card.addView(sliderLayout);
+        }
+    }
 }
 
 
@@ -253,114 +285,5 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
 
 
 
-    /*public void renewItems(View view) {
-       *//* List<SliderItem> sliderItemList = new ArrayList<>();*//*
-        //dummy data
-        for (int i = 0; i < 5; i++) {
-            SliderItem sliderItem = new SliderItem();
-            sliderItem.setDescription("Slider Item " + i);
-            if (i % 2 == 0) {
-               // sliderItem.setImageUrl(R.drawable.image3);
-            } else {
-                //sliderItem.setImageUrl(R.drawable.image2);
-            }
-            sliderItemList.add(sliderItem);
-        }
-        adapter.renewItems(sliderItemList);
-    }
-
-    public void removeLastItem(View view) {
-        if (adapter.getCount() - 1 >= 0)
-            adapter.deleteItem(adapter.getCount() - 1);
-    }
-
-    public void addNewItem(View view) {
-
-
-        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-                return;
-        }
-
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setType("image/*");
-        startActivityForResult(intent, 1);
-
-
-*//*        SliderItem sliderItem = new SliderItem();
-        sliderItem.setDescription("Slider Item Added Manually");
-        sliderItem.setImageUrl(R.drawable.image1);
-        adapter.addItem(sliderItem);*//*
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
-        Log.d("mainActivity", "we r here");
-
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            final List<Bitmap> bm = new ArrayList<>();
-            Log.d("mainActivity", "we r in requestcode");
-
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-                ClipData clipData = data.getClipData();
-                Log.d("mainActivity", "we checked version");
-                if(clipData != null){
-                    Toast.makeText(MainActivity.this, "its far", Toast.LENGTH_SHORT).show();
-                    Log.d("mainActivity", "we r cliping data");
-                    for(int i = 0; i<clipData.getItemCount(); i++){
-                        Uri imgurl = clipData.getItemAt(i).getUri();
-                        try {
-                            InputStream is = getContentResolver().openInputStream(imgurl);
-                            Bitmap bitmap = BitmapFactory.decodeStream(is);
-                            bm.add(bitmap);
-
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-
-                else {
-                    Uri imageUrl  = data.getData();
-
-                    try {
-                        InputStream is = getContentResolver().openInputStream(imageUrl);
-                        Bitmap bitmap = BitmapFactory.decodeStream(is);
-                        bm.add(bitmap);
-
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for(final Bitmap b: bm){
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Log.d("mainActivity", "we r in thread");
-                                    SliderItem sliderItem = new SliderItem();
-                                    sliderItem.setImageUrl(b);
-                                    sliderItemList.add(sliderItem);
-                                    sliderItem.setDescription("Slider Item Added Manually");
-                                    adapter.renewItems(sliderItemList);
-                                }
-                            });
-
-                        }
-
-                    }
-                }).start();
-
-        }
-    }
-*/
 
 
---
+++ app/src/main/java/com/example/autoslideshow/PortraitModeFrag.java
@@ -0,0 +1,43 @@
+package com.example.autoslideshow;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class PortraitModeFrag extends Fragment {
+
+    public PortraitModeFrag() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_portrait_mode, container, false);
+
+
+
+        return v;
+
+    }
+
+
+
+
+
+
+}
